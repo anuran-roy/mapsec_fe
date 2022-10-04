2400f495ce34d41229e62d78f5dd30e26738aebd
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-04 14:39:11 +0530

    Reformatted code

--- src/components/website/scans/scanDetails.tsx
+++ src/components/website/scans/scanDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function ScanDetails(props: any) {
   const scanId: any = props.scanId;
@@ -55,32 +55,25 @@ function ScanDetails(props: any) {
     },
   };
 
-  const getScanDetails: any = async (scanId: number) => {
-    const res = await fetch(
-      `http://localhost:8000/api/v1/scanner/info/${scanId}`,
-      {
-        method: "GET",
-        mode: "cors",
-        // headers: headers
-      }
-    );
-
-    const jsonData = await res.json();
-
-    return jsonData;
-  };
-
   const [threatReport, setThreatReport] = useState<any>(defaultScanDetails);
 
-  if (threatReport === defaultScanDetails) {
-    getScanDetails(scanId)
-      // .then((results: any) => results.json())
-      .then((results: any) => setThreatReport(results));
-  }
+  const getScanDetails: any = () => {
+    fetch(`http://localhost:8000/api/v1/scanner/info/${scanId}`, {
+      method: "GET",
+      mode: "cors",
+      // headers: headers
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setThreatReport(res);
+      });
+  };
+
+  useEffect(() => getScanDetails(), []);
 
   //   console.log(threatReport);
   return (
-    <>
+    <div className="rounded-md">
       <div className="rounded-md m-5 p-3 bg-white w-100">
         <h2 className="font-bold text-left px-3 text-2xl text-gray-600">
           {threatReport?.name}
@@ -90,74 +83,69 @@ function ScanDetails(props: any) {
         </div>
       </div>
       <div className="grid grid-cols-3">
-        <div className="col-span-1 p-3 mx-5 bg-white">
-          <h3 className="font-bold text-left px-3 text-2xl text-gray-600">
-            Stats for {threatReport?.target_device.name}
+        <div className="rounded-md col-span-1 p-3 mx-5 bg-white">
+          <h3 className="font-bold text-left p-3 m-3 text-2xl text-gray-600">
+            Stats for {threatReport?.target_device?.name}
           </h3>
+          <div className="text-left self-justify-center m-3 px-3">
+            {Object.keys(threatReport?.target_device).map((ele: any) => {
+              if (ele !== "metadata") {
+                return (
+                  <div className="my-3">
+                    <div className="font-bold capitalize">{ele}</div>
+                    <div>{threatReport?.target_device[ele]}</div>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="my-3">
+                    <div className="font-bold">Metadata </div>
+                    {Object.keys(threatReport?.target_device?.metadata).map(
+                      (ele: any) => (
+                        <div>
+                          {ele}: {threatReport?.target_device?.metadata[ele]}
+                        </div>
+                      )
+                    )}
+                  </div>
+                );
+              }
+            })}
+          </div>
         </div>
-        <div className="col-span-2 p-3 mx-5 bg-white">
-          <h3 className="font-bold text-left px-3 text-2xl text-gray-600">
-            Stats for {threatReport?.target_device.name}
+        <div className="rounded-md col-span-2 p-3 mx-5 bg-white">
+          <h3 className="capitalize font-semibold text-2xl text-gray-700 py-3 my-3">
+            Report for {threatReport?.target_device?.name}
           </h3>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minima
-            voluptates nisi repudiandae. Ipsum saepe nemo itaque quaerat natus
-            porro, velit sequi dolore, repudiandae nam neque, aspernatur est
-            dolorem sunt corrupti distinctio ex maiores nulla incidunt. Omnis
-            blanditiis, aliquam, voluptatibus eveniet voluptas tempora autem
-            quis quisquam, nobis harum vitae sapiente.
-          </div>
+          {Object.keys(threatReport?.report).map((ele: any, idx: number) => {
+            return (
+              <div key={`report-${idx}`} className="mx-5 px-5 my-3 py-3">
+                <h4 className="capitalize font-semibold text-xl text-gray-700">
+                  {ele}
+                </h4>
+                <div>Count: {threatReport?.report[ele]?.count}</div>
+                <div>
+                  {threatReport?.report[ele]?.details.map((det: any) => {
+                    return (
+                      <div className="text-left">
+                        {Object.keys(det).map((key) => {
+                          return (
+                            <div className="flex flex-row">
+                              <div className="font-bold capitalize">{key}</div>:{" "}
+                              {det[key]}
+                            </div>
+                          );
+                        })}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
