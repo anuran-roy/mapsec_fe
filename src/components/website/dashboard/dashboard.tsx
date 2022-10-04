44988d620b58024671f2c7c8c471154bb3b3355f
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-04 13:56:34 +0530

    Added graph rendering loader, modified graph to plot data from an endpoint

--- src/components/website/dashboard/dashboard.tsx
+++ src/components/website/dashboard/dashboard.tsx
@@ -1,33 +1,82 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Menu from "../menu";
 import NavBar from "../navbar";
 import ScanCard from "../scans/scancard";
 import InsertJoke from "../jokes/jokes";
-import Plot from 'react-plotly.js';
-
+import Plot from "react-plotly.js";
 
 function Dashboard() {
-  const [currentComponent, setCurrentComponent] = useState<any>("");
+  const [analyticsArrived, setAnalyticsArrived] = useState<boolean>(false);
+  const [analyticsData, setAnalyticsData] = useState<any>([
+    [1, 2, 3],
+    [2, 6, 3],
+  ]);
+
+  const getAnalytics = () => {
+    fetch("http://localhost:8000/api/v1/analytics/scanhistory", {
+      method: "GET",
+      mode: "cors",
+      // body: JSON.stringify(scanDetails),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setAnalyticsArrived(true);
+        console.log(res);
+        setAnalyticsData(res);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => getAnalytics(), [analyticsArrived === false]);
 
   return (
     <>
       <div id="joke" className="italic font-bold">
         <InsertJoke />
       </div>
-      <Plot
-        className="m-5"
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            // marker: {color: '22c55e'},
-          },
-          // {type: 'bar', marker: {color: "22c55e"}, x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {showlegend: false, paper_bgcolor: "f3f4f6", plot_bgcolor: "f3f4f6", width: 1280, height: 720, title: 'Vulnerabilities count in weekly basis'} }
-      />
+      <div className="max-w-screen">{analyticsArrived ? (<Plot
+          className="m-5"
+          data={[
+            {
+              x: analyticsData[0],
+              y: analyticsData[1],
+              type: "scatter",
+              mode: "lines+markers",
+              // marker: {color: '22c55e'},
+            },
+            // {type: 'bar', marker: {color: "22c55e"}, x: [1, 2, 3], y: [2, 5, 3]},
+          ]}
+          layout={{
+            yaxis: {
+              tickmode: "linear",
+              tick0: 0,
+            },
+            // barmode: "stack",
+            showlegend: false,
+            paper_bgcolor: "f3f4f6",
+            plot_bgcolor: "f3f4f6",
+            width: 1280,
+            height: 720,
+            // responsive: true,
+            title: "Vulnerabilities count across time",
+          }}
+        />): (<div className="rounded-md p-4 max-w-6xl w-full mx-auto">
+        <div className="animate-pulse flex space-x-4">
+          {/* <div className="rounded-full bg-slate-200 h-10 w-10"></div> */}
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-96 mx-6 my-3 rounded-md bg-slate-200 rounded text-4xl font-bold align-middle flex flex-col text-slate-400 justify-center">Getting Analytics...</div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                {/* <div className="h-2 bg-slate-200 rounded col-span-2">Getting Analytics</div> */}
+                {/* <div className="h-2 bg-slate-200 rounded col-span-1"></div> */}
+              </div>
+              {/* <div className="h-2 bg-slate-200 rounded"></div> */}
+            </div>
+          </div>
+        </div>
+      </div>)}
+      </div>
       <br />
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
       maxime.
