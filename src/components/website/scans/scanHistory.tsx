c71ae0ad28ab1580e16c5f030f18581f31be8855
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-04 14:00:17 +0530

    Added keys for children coming from array

--- src/components/website/scans/scanHistory.tsx
+++ src/components/website/scans/scanHistory.tsx
@@ -19,8 +19,8 @@ function ScanHistory(props: any) {
 
   return (
     <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-100 mx-5 px-5">
-      {resultArrived.ids.map((ele: number) => (
-        <ScanCard key={ele} scanId={ele}></ScanCard>
+      {resultArrived.ids.map((ele: any, idx: number) => (
+        <ScanCard key={`card-${idx}`} scanId={ele} />
       ))}
     </div>
   );
