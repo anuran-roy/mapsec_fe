be85bdfbadcfd13c54f886da22e49317751521db
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-05 18:43:16 +0530

    Created about page and added it to navbar

---
+++ src/components/website/about.tsx
@@ -0,0 +1,5 @@
+export default function About() {
+    return (<>
+        <h2 className="flex flex-col justify-center align-middle h-screen items-center font-bold text-slate-700 text-4xl">Made by Anuran with 💙</h2>
+    </>);
+}
