7ae6be85b3199d01bd31c774fbff9a54dddac835
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-05 18:45:39 +0530

    Added routes for About and Account pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,9 +5,13 @@ import ScanDetails from "../components/website/scans/scanDetails";
 import NotFound404 from "../components/website/errors/not_found";
 import ComingSoon from "../components/website/errors/coming_soon";
 import NewScan from "../components/website/scans/newScan";
+import About from "../components/website/about";
+import Account from "../components/website/account/account";
 
 const routes = {
   "/": () => <Redirect to="/web/dashboard"></Redirect>,
+  "/web/about": () => <About />,
+  "/web/account": () => <Account />,
   "/web/dashboard": () => <Dashboard></Dashboard>,
   "/web/scans": () => <ScanHistory></ScanHistory>,
   "/web/scans/new": () => <NewScan />,
