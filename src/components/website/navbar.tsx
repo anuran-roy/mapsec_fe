1494ea7c8aeeea6f3572368caa358d10db647328
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-05 18:10:11 +0530

    Tweaked spacing

--- src/components/website/navbar.tsx
+++ src/components/website/navbar.tsx
@@ -1,12 +1,18 @@
 function NavBar(props: any) {
   return (
-    <div className="navbar bg-gray-100 flex flex-row sticky top-0 z-50 shadow-md">
-      <h1 className="capitalize text-4xl top-0 px-5 py-4 font-bold text-gray-600 justify-self-center text-left">
+    <div className="navbar py-3 bg-gray-100 flex flex-row sticky top-0 z-50 shadow-md">
+      <h1 className="capitalize text-4xl top-0 px-5 py-1 font-bold text-gray-600 justify-self-center text-left">
         {props.page}
       </h1>
-      <div className="w-100"></div>
-      <div className="right-0 top-0 py-5 px-3 text-gray-700 font-medium">
+      <div className="max-w-7xl w-full"></div>
+      <div className="flex flex-row align-middle">
+      <a href="/web/account" className="right-0 top-0 my-2 py-1 px-3 flex flex-col justify-center rounded-md text-gray-700 font-medium hover:bg-gray-700 hover:text-white">
         Account
+      </a>
+      <a href="/web/about" className="right-0 top-0 my-2 py-1 px-3 flex flex-col justify-center rounded-md text-gray-700 font-medium hover:bg-gray-700 hover:text-white">
+        About
+      </a>
+      <div className="w-32"></div>
       </div>
     </div>
   );
